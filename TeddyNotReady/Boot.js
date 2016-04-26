da1ecac0f63a0ac60ea3a4741785010d15d429f9
dvanlunen <d.vanlunen@gmail.com>
2016-04-25 22:09:18 -0400

    Game changes
    Old men collide

---
+++ TeddyNotReady/Boot.js
@@ -0,0 +1,50 @@
+var BasicGame = {};
+
+BasicGame.Boot = function (game) {
+
+};
+
+BasicGame.Boot.prototype = {
+
+    init: function () {
+        //  Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
+        this.input.maxPointers = 1;
+
+        //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
+        this.stage.disableVisibilityChange = true;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.setMinMax(400, 400, 1000, 1000);
+        this.scale.forceLandscape = true;
+        this.scale.pageAlignHorizontally = true;
+
+        if (this.game.device.desktop)
+        {
+            this.scale.pageAlignHorizontally = true;
+
+        }
+        else
+        {
+            //  Same goes for mobile settings.
+            //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        }
+
+    },
+
+    preload: function () {
+
+        //  Here we load the assets required for our preloader (in this case a background and a loading bar)
+        this.load.image('Background', 'images/grass.png');
+        this.load.image('preloaderBar', 'images/loadbar.png');
+
+    },
+
+    create: function () {
+
+        //  By this point the preloader assets have loaded to the cache, we've set the game settings
+        //  So now let's start the real preloader going
+        this.state.start('Preloader');
+
+    }
+
+};
---
+++ TeddyNotReady/Game.js
@@ -0,0 +1,197 @@
+
+BasicGame.Game = function (game) {
+
+    //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+
+    this.game;      //  a reference to the currently running game (Phaser.Game)
+    this.add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
+    this.camera;    //  a reference to the game camera (Phaser.Camera)
+    this.cache;     //  the game cache (Phaser.Cache)
+    this.input;     //  the global input manager. You can access this.input.keyboard, this.input.mouse, as well from it. (Phaser.Input)
+    this.load;      //  for preloading assets (Phaser.Loader)
+    this.math;      //  lots of useful common math operations (Phaser.Math)
+    this.sound;     //  the sound manager - add a sound, play one, set-up markers, etc (Phaser.SoundManager)
+    this.stage;     //  the game stage (Phaser.Stage)
+    this.time;      //  the clock (Phaser.Time)
+    this.tweens;    //  the tween manager (Phaser.TweenManager)
+    this.state;     //  the state manager (Phaser.StateManager)
+    this.world;     //  the game world (Phaser.World)
+    this.particles; //  the particle manager (Phaser.Particles)
+    this.physics;   //  the physics manager (Phaser.Physics)
+    this.rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
+    //  You can use any of these from any function within this State.
+    //  But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
+
+};
+
+
+
+
+Enemy = function(index, game, player, bullets, health, image, speed, firerate){
+    // random spawn locations at least 100 away from player start
+    var x = game.rnd.integerInRange(1100, 2900) % 2000;
+    var y = game.rnd.integerInRange(1100, 2900) % 2000;
+
+    this.game = game;
+    this.health = health;
+    this.player = player;
+    this.bullets = bullets;
+    this.fireRate = firerate;
+    this.nextFire = 0;
+    this.alive = true;
+    this.speed = speed;
+    this.image = game.add.sprite(x, y, image);
+    enemiesGroup.add(this.image);
+    console.log(enemiesGroup);
+
+
+    this.image.anchor.set(0.5);
+    this.image.name = index.toString();
+    this.game.physics.enable(this.image, Phaser.Physics.ARCADE);
+    this.image.body.immovable = false;
+    this.image.body.collideWorldBounds = true;
+    this.image.body.bounce.setTo(1, 1);
+    this.image.rotation = this.game.physics.arcade.angleBetween(this.image, this.player);
+    this.game.physics.arcade.velocityFromRotation(this.image.rotation, this.speed, this.image.body.velocity);
+};
+
+Enemy.prototype.update = function() {
+    this.image.rotation = this.game.physics.arcade.angleBetween(this.image, this.player);
+    this.game.physics.arcade.velocityFromRotation(this.image.rotation, this.speed, this.image.body.velocity);
+};
+
+var player;
+var currentSpeed = 0;
+var barks;
+var nextFire = 0;
+var fireRate = 400;
+
+var enemies;
+var enemyBullets;
+var enemiesTotal = 0;
+var enemiesAlive = 0;
+var enemiesGroup;
+
+BasicGame.Game.prototype = {
+
+    create: function () {
+        this.game.world.setBounds(0, 0, 2000, 2000);
+        grass = this.game.add.tileSprite(0, 0, 800, 800,'Background');
+        grass.fixedToCamera = true;
+
+        // setup player
+        player = this.game.add.sprite(1000, 1000, 'player');
+        player.anchor.setTo(0.5, 0.5);
+        player.animations.add('moving', [0, 1, 2], 15, true);
+        this.game.physics.enable(player,Phaser.Physics.ARCADE);
+        player.body.drag.set(0.2);
+        player.body.maxVelocity.setTo(400,400);
+        player.body.collideWorldBounds = true;
+        barks = this.game.add.group();
+        barks.enableBody = true;
+        barks.physicsBodyType = Phaser.Physics.ARCADE;
+        barks.createMultiple(30, 'bark', 0, false);
+        barks.setAll('anchor.x', 0.5);
+        barks.setAll('anchor.y', 0.5);
+        barks.setAll('outOfBoundsKill', true);
+        barks.setAll('checkWorldBounds', true);
+
+        enemies = [];
+        enemiesTotal = 20;
+        enemiesAlive = 20;
+        enemiesGroup = this.game.add.physicsGroup();
+
+        for (var i = 0; i < enemiesTotal; i++) {
+            enemies.push(new Enemy(i, this.game, player, enemyBullets, 3, 'oldman', 100, 100));
+
+        }
+
+        player.bringToTop();
+
+        this.game.camera.follow(player);
+        this.game.camera.focusOnXY(0, 0);
+
+        cursors = this.game.input.keyboard.createCursorKeys();
+
+        
+
+    },
+
+    update: function () {
+
+    this.game.physics.arcade.collide(enemiesGroup, enemiesGroup);
+        // player.body.velocity.x = 0;
+    if (cursors.left.isDown)
+    {
+        player.angle -= 4;
+    }
+    else if (cursors.right.isDown)
+    {
+        player.angle += 4;
+    }
+
+    if (cursors.up.isDown)
+    {
+        //  The speed we'll travel at
+        currentSpeed = 300;
+        player.animations.play('moving');
+    }
+    else
+    {
+        if (currentSpeed > 0)
+        {
+            currentSpeed -= 4;
+        }
+    }
+
+    if (currentSpeed >= 0)
+    {
+        this.game.physics.arcade.velocityFromRotation(player.rotation, currentSpeed, player.body.velocity);
+        if (currentSpeed === 0){
+            player.animations.stop();
+            player.frame = 1;
+        }
+    }
+
+
+    if (this.game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)){
+        this.fire();
+    }
+
+    //enemies move toward player
+    enemiesAlive = 0;
+    for (var i = 0; i < enemies.length; i++){
+        if (enemies[i].alive){
+            enemiesAlive++;
+            enemies[i].update();
+        }
+    }
+
+
+
+    grass.tilePosition.x = -this.game.camera.x;
+    grass.tilePosition.y = -this.game.camera.y;
+    },
+
+    fire: function () {
+        if (this.game.time.now > nextFire && barks.countDead() > 0){
+            nextFire = this.game.time.now + fireRate;
+            var bark = barks.getFirstExists(false);
+            bark.reset(player.x, player.y);
+            bark.rotation = player.rotation;
+            this.game.physics.arcade.velocityFromRotation(bark.rotation, 600, bark.body.velocity);
+        }
+    },
+
+
+    quitGame: function (pointer) {
+
+        //  Here you should destroy anything you no longer need.
+        //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
+
+        //  Then let's go back to the main menu.
+        // this.game.state.start('MainMenu');
+
+    }
+
+};
