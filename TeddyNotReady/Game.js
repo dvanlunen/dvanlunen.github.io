0ec54bd7713a427de377251dc65b74f7023e8696
dvanlunen <d.vanlunen@gmail.com>
2016-05-01 11:33:26 -0400

    Game Edits
    Diff enemy classes, player hurt, game over, etc.

--- TeddyNotReady/Game.js
+++ TeddyNotReady/Game.js
@@ -24,134 +24,139 @@ BasicGame.Game = function (game) {
 
 };
 
-
-
-
-
-
-
-// Enemy parent class
-function Enemy(index, game, player, bullets){
+// enemy super class
+function Enemy(index, game, player, locslot){
+    this.index = index;
     this.game = game;
     this.player = player;
-    this.bullet = bullets;
     this.alive = true;
-    var x = game.rnd.integerInRange(1100, 2900) % 2000;
-    var y = game.rnd.integerInRange(1100, 2900) % 2000;
     this.health = 0;
-    this.image = null;
+    this.speed = 0;
+    this.animspeed = 15;
+    this.locslot = locslot;
+    // this.deathSound = this.game.oldManDeadSound;
 }
+// locslot is an integer between 0 and 323=18^2-1
+// that determines spawn locations
 
-Enemy.prototype.damage = function(damage){
-    this.health -= damage;
-    if (this.health <=0){
-        this.alive = false;
-        this.image.kill();
-        return true;
-    }
-    return false;
-};
-
-Enemy.prototype.update = function() {
-    this.image.rotation = this.game.physics.arcade.angleBetween(this.image, this.player);
-    this.game.physics.arcade.velocityFromRotation(this.image.rotation, this.speed, this.image.body.velocity);
-};
-
-function oldMan(){
-    Enemy.call(this);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Enemy = function(index, game, player, bullets, health, image, speed, firerate){
-    // random spawn locations at least 100 away from player start
-    var x = game.rnd.integerInRange(1100, 2900) % 2000;
-    var y = game.rnd.integerInRange(1100, 2900) % 2000;
-
-    this.game = game;
-    this.health = health;
-    this.player = player;
-    this.bullets = bullets;
-    this.fireRate = firerate;
-    this.nextFire = 0;
-    this.alive = true;
-    this.speed = speed;
-    this.image = game.add.sprite(x, y, image);
-    this.image.animations.add('oldmanmoves', [0, 1, 2], 15, true);
-    this.image.animations.play('oldmanmoves');
-    enemiesGroup.add(this.image);
-
-
-
-    this.image.anchor.set(0.5);
-    this.image.name = index.toString();
-    this.game.physics.enable(this.image, Phaser.Physics.ARCADE);
-    this.image.body.immovable = false;
-    this.image.body.collideWorldBounds = true;
-    this.image.body.bounce.setTo(1, 1);
-    this.image.rotation = this.game.physics.arcade.angleBetween(this.image, this.player);
-    this.game.physics.arcade.velocityFromRotation(this.image.rotation, this.speed, this.image.body.velocity);
+Enemy.prototype.addEnemySprite = function(spritename, animationname){
+    var x = (((this.locslot % 18) * 100) + 1100) % 2000;
+    var y = ((100 * Math.floor(this.locslot / 18)) + 1100) % 2000;
+    this.enemy = this.game.add.sprite(x, y, spritename);
+    enemiesGroup.add(this.enemy);
+    this.enemy.anchor.set(0.5);
+    this.enemy.name = this.index.toString();
+    this.game.physics.enable(this.enemy, Phaser.Physics.ARCADE);
+    this.enemy.body.immovable = false;
+    this.enemy.body.collideWorldBounds = true;
+    this.enemy.body.bounce.setTo(2, 2);
+    this.enemy.animations.add(animationname, [0, 1, 2, 1], this.animspeed, true);
+    this.enemy.animations.play(animationname);
+    this.value = 0;
 };
 
 Enemy.prototype.damage = function(damage){
     this.health -= damage;
     if (this.health <=0){
         this.alive = false;
-        this.image.kill();
+        this.enemy.kill();
+        bones = bones + this.value;
+        // this.deathSound.play();
         return true;
     }
     return false;
 };
 
 Enemy.prototype.update = function() {
-    this.image.rotation = this.game.physics.arcade.angleBetween(this.image, this.player);
-    this.game.physics.arcade.velocityFromRotation(this.image.rotation, this.speed, this.image.body.velocity);
+    this.enemy.rotation = this.game.physics.arcade.angleBetween(this.enemy, this.player);
+    this.game.physics.arcade.velocityFromRotation(this.enemy.rotation, this.speed, this.enemy.body.velocity);
 };
 
+// Enemy Children classes
+
+// Old Man
+function oldMan(index, game, player, locslot){
+    Enemy.call(this, index, game, player, locslot);
+    this.health = 1;
+    this.speed = 70;
+    this.animspeed = 5;
+    this.addEnemySprite('oldman','oldmanmove');
+    this.value = 1;
+    // this.deathSound = oldManDeadSound;
+}
+oldMan.prototype = Object.create(Enemy.prototype);
+oldMan.prototype.constructor = oldMan;
+
+// Man
+function Man(index, game, player, locslot){
+    Enemy.call(this, index, game, player, locslot);
+    this.health = 2;
+    this.speed = 100;
+    this.animspeed = 10;
+    this.addEnemySprite('man','manmove');
+    this.value = 3;
+    // this.deathSound = manDeadSound;
+}
+Man.prototype = Object.create(Enemy.prototype);
+Man.prototype.constructor = Man;
+
+// bones
+var bones = 0;
+var bonesText;
 
 
-
+// player
 var player;
 var currentSpeed = 0;
+var playerMaxHealth = 3;
+var nextHurt = 0;
+var playerHealth;
+var score = 0;
+
+//barks
 var barks;
 var nextFire = 0;
 var fireRate = 400;
 var barkDamage = 1;
-var bones = 0;
 
+// enemies
 var enemies;
-var enemyBullets;
 var enemiesTotal = 0;
 var enemiesAlive = 0;
 var enemiesGroup;
+var enemyBullets;
+// var oldManDeadSound;
+// var manDeadSound;
+
+// had to create a variable to story a time condition to stop displaying game over and go back to main menu
+var endGame;
+
+
 
 BasicGame.Game.prototype = {
 
     create: function () {
-        this.game.world.setBounds(0, 0, 2000, 2000);
-        grass = this.game.add.tileSprite(0, 0, 800, 800,'Background');
+        endGame = Infinity;
+        this.world.setBounds(0, 0, 2000, 2000);
+        grass = this.add.tileSprite(0, 0, 800, 800,'Background');
         grass.fixedToCamera = true;
 
         // setup player
-        player = this.game.add.sprite(1000, 1000, 'player');
+        player = this.add.sprite(1000, 1000, 'player');
         player.anchor.setTo(0.5, 0.5);
-        player.animations.add('moving', [0, 1, 2], 15, true);
-        this.game.physics.enable(player,Phaser.Physics.ARCADE);
+        player.animations.add('moving', [0, 1, 2, 1], 15, true);
+        player.animations.add('hurtmoving', [0, 3, 1, 3, 2, 3, 1] , 15, true);
+        player.animations.add('hurtstill', [1, 3, 1] , true);
+        this.physics.enable(player,Phaser.Physics.ARCADE);
         player.body.drag.set(0.2);
         player.body.maxVelocity.setTo(400,400);
         player.body.collideWorldBounds = true;
-        barks = this.game.add.group();
+        playerHealth = playerMaxHealth;
+        // hurtSound = this.add.audio('playerhurt');
+        // dieSound = this.add.audio('playerdead');
+
+        //setup barks
+        barks = this.add.group();
         barks.enableBody = true;
         barks.physicsBodyType = Phaser.Physics.ARCADE;
         barks.createMultiple(30, 'bark', 0, false);
@@ -160,65 +165,110 @@ BasicGame.Game.prototype = {
         barks.setAll('outOfBoundsKill', true);
         barks.setAll('checkWorldBounds', true);
 
+        //setup enemies
+        // oldManDeadSound = this.add.audio('oldmandead');
+        // manDeadSound = this.add.audio('mandead');
+
         enemies = [];
         enemiesTotal = 20;
         enemiesAlive = 20;
-        enemiesGroup = this.game.add.physicsGroup();
+        enemiesGroup = this.add.group();
+        enemiesGroup.enableBody = true;
+        enemiesGroup.physicsBodyType = Phaser.Physics.ARCADE;
 
-        for (var i = 0; i < enemiesTotal; i++) {
-            enemies.push(new Enemy(i, this.game, player, enemyBullets, 1, 'oldman', 50, 100));
+        // create an array of slots that determine enemy spawn location
+        var startingloc = 0;
+        var locbucket = [];
+        while (startingloc <= 323){
+            locbucket.push(startingloc++);
+        }
+
+        // spawn enemies
+        var randomloc = 0;
+        for (var i = 0; i < 10; i++) {
+            randomloc = Math.floor(Math.random()*locbucket.length);
+            startingloc = locbucket.splice(randomloc, 1)[0];
+            enemies.push(new oldMan(i, this.game, player,startingloc));
+
+        }
+        for (i = 10; i < enemiesTotal; i++) {
+            randomloc = Math.floor(Math.random()*locbucket.length);
+            startingloc = locbucket.splice(randomloc, 1)[0];
+            enemies.push(new Man(i, this.game, player,startingloc));
 
         }
 
+
+        // visuals
         player.bringToTop();
+        this.camera.follow(player);
+        this.camera.focusOnXY(0, 0);
+        healthbar = this.add.sprite(this.camera.x, this.camera.y, playerHealth + 'heart');
+        bone = this.add.sprite(this.camera.x + 760, this.camera.y + 5, 'bone');
+        bonesText = this.add.text(this.camera.x + 755, this.camera.y + 3,
+                    bones, {font: "32px Arial", align: "right"});
+        bonesText.anchor.set(1, 0);
 
-        this.game.camera.follow(player);
-        this.game.camera.focusOnXY(0, 0);
-
-        cursors = this.game.input.keyboard.createCursorKeys();
-
+        // setup keys
+        cursors = this.input.keyboard.createCursorKeys();
         
 
     },
 
     update: function () {
+    if (this.time.now > endGame){
+        this.quitGame();
+    }
+    healthbar.x = this.camera.x + 5;
+    healthbar.y = this.camera.y + 5;
+    bonesText.x = this.camera.x + 755;
+    bonesText.y = this.camera.y + 3;
+    bonesText.text = bones;
+    bone.x = this.camera.x + 760;
+    bone.y = this.camera.y + 5;
 
-    this.game.physics.arcade.collide(enemiesGroup, enemiesGroup);
-        // player.body.velocity.x = 0;
-    if (cursors.left.isDown)
-    {
+
+
+    this.physics.arcade.collide(enemiesGroup, enemiesGroup);
+    if (cursors.left.isDown){
         player.angle -= 4;
     }
-    else if (cursors.right.isDown)
-    {
+    else if (cursors.right.isDown){
         player.angle += 4;
     }
 
-    if (cursors.up.isDown)
-    {
+    if (cursors.up.isDown){
         //  The speed we'll travel at
         currentSpeed = 300;
-        player.animations.play('moving');
+        if (this.time.now > nextHurt){
+            player.animations.play('moving');
+        }
+        else {
+           player.animations.play('hurtmoving');
+        }
     }
-    else
-    {
-        if (currentSpeed > 0)
-        {
+    else {
+        if (currentSpeed > 0) {
             currentSpeed -= 4;
         }
     }
 
     if (currentSpeed >= 0)
     {
-        this.game.physics.arcade.velocityFromRotation(player.rotation, currentSpeed, player.body.velocity);
+        this.physics.arcade.velocityFromRotation(player.rotation, currentSpeed, player.body.velocity);
         if (currentSpeed === 0){
-            player.animations.stop();
-            player.frame = 1;
+            if (this.time.now > nextHurt){
+                player.animations.stop();
+                player.frame = 1;
+            }
+            else {
+                player.animations.play('hurtmoving');
+            }
+            
         }
     }
 
-
-    if (this.game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)){
+    if (this.input.activePointer.isDown){
         this.fire();
     }
 
@@ -227,43 +277,72 @@ BasicGame.Game.prototype = {
     for (var i = 0; i < enemies.length; i++){
         if (enemies[i].alive){
             enemiesAlive++;
-            this.physics.arcade.overlap(barks, enemies[i].image, this.barkHitEnemy, null, this);
+            this.physics.arcade.overlap(barks, enemies[i].enemy, this.barkHitEnemy, null, this);
             enemies[i].update();
         }
     }
 
+  
+    this.physics.arcade.overlap(player, enemiesGroup, this.playerHit, null, this);
+    
+   
+    grass.tilePosition.x = -this.camera.x;
+    grass.tilePosition.y = -this.camera.y;
 
-
-    grass.tilePosition.x = -this.game.camera.x;
-    grass.tilePosition.y = -this.game.camera.y;
     },
 
     fire: function () {
-        if (this.game.time.now > nextFire && barks.countDead() > 0){
-            nextFire = this.game.time.now + fireRate;
+        if (this.time.now > nextFire && barks.countDead() > 0){
+            nextFire = this.time.now + fireRate;
             var bark = barks.getFirstExists(false);
             bark.reset(player.x, player.y);
-            bark.rotation = player.rotation;
-            this.game.physics.arcade.velocityFromRotation(bark.rotation, 600, bark.body.velocity);
+            bark.rotation = this.physics.arcade.moveToPointer(bark, 400, this.input.activePointer);
         }
     },
 
     barkHitEnemy: function (enemy, bark){
         bark.kill();
         var destroyed = enemies[enemy.name].damage(barkDamage);
-        // if (destroyed){
-         //   var
-        //}
     },
 
+    playerHit: function (player, enemy){
+        if (this.time.now > nextHurt){
+            nextHurt = this.time.now + 2000;
+            playerHealth--;
+            if (playerHealth === 0){
+                // dieSound.play();
+                gameOverText = this.add.bitmapText(this.camera.x + 100, this.camera.y + 250,
+                    'gooddog', 'GAME OVER', 100);
+                gameOverText.updateTransform();
+                gameOverText.position.x = this.camera.x + 400 - gameOverText.textWidth / 2;
+                gameOverText.position.y = this.camera.y + 300 - gameOverText.textHeight / 2;
+
+
+                //text(this.camera.x + 100, this.camera.y + 250, "GAME OVER",
+                    //{font: 'gooddog', fontSize: '100px', align: 'center'});
+                player.kill();
+                healthbar.kill();
+                endGame = this.time.now + 3000;
+            } else {
+                // hurtSound.play();
+                healthbar.loadTexture(playerHealth + 'heart');
+                if (playerHealth === 1) {
+                    healthbar.animations.add('lastheart', [0, 1, 2], 5, true);
+                    healthbar.animations.play('lastheart');
+                }
+            }
+            
+        }
+
+    },
 
     quitGame: function (pointer) {
-
-        //  Here you should destroy anything you no longer need.
-        //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
-
+        
+        for (var i = 0; i < enemies.length; i++){
+            enemies[i].enemy.kill();
+        }
         //  Then let's go back to the main menu.
-        // this.game.state.start('MainMenu');
+        this.state.start('MainMenu');
 
     }
 
